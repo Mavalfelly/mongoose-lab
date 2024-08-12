c5a4c9275a58ac89bf2013323cfad329457087e1
MaValFelly <matthewfeliciano16@gmail.com>
2024-08-12 17:51:34 -0400

    final

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const Custy = require('./models/customer.js')
 const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
 //landing page//
 app.get('/',(req,res)=>{
     res.render('home.ejs')
@@ -27,16 +26,6 @@ app.post('/cust', async (req,res)=>{
         res.status(400).json(err)
     }
 });
-
-//EJS
-app.post('/cust-ejs', async (req,res)=>{
-    try{
-        await Custy.create(req.body)
-        res.redirect('/')
-    }catch (err){
-        res.status(400).json(err)
-    }
-});
 app.put('/cust/:id', async (req,res)=>{
     try{
         res.json(await Custy.findByIdAndUpdate(req.params.id, req.body))
@@ -52,6 +41,25 @@ app.delete('/cust/:id', async (req,res)=>{
         res.status(400).json(err)
     }
 });
+//EJS
+app.post('/cust-ejs', async (req,res)=>{
+    try{
+        await Custy.create(req.body)
+        res.redirect('/')
+    }catch (err){
+        res.status(400).json(err)
+    }
+});
+app.get('/cust-ejs', async (req,res)=>{
+    try{
+        let x= await Custy.find(({}))
+        console.log(x)
+        res.render('./partials/custys.ejs', {x})
+    }catch (err){
+        res.status(400).json(err)
+    }
+});
+
 
 
 
